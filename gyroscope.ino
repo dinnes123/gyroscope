0b3bccd79da48c4385f9bc1f2ec9bf3e90dc6f2e
張恩典 <dinnesdenver@gmail.com>
2017-05-29 15:02:23 +0800

    Add files via upload

--- gyroscope.ino
+++ gyroscope.ino
@@ -1,13 +1,11 @@
 #include "gyroscope.h"
-gyroscope_mpu6050 gyroscope_mpu6050_1;
-void setup() 
-    {
-          Serial.begin(9600);
-    }
-void loop() 
-    {
-          gyroscope_mpu6050_1.get_v();  //取得陀螺儀的角度
-          Serial.print("z axis=");Serial.println( gyroscope_mpu6050_1.z_axis );  //顯示z軸的值
-          Serial.print("x axis=");Serial.println( gyroscope_mpu6050_1.x_axis );  //顯示x軸的值
-          Serial.print("y axis=");Serial.println( gyroscope_mpu6050_1.y_axis );  //顯示y軸的值
-    }
+gyroscope_mpu6050 gyroscope_mpu6050_1(11,10,115200);
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(gyroscope_mpu6050_1.get_v());
+}
