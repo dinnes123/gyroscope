8bd5b20fe2c4757c572769c106f6b664bb2bd5f5
張恩典 <dinnesdenver@gmail.com>
2017-05-29 15:03:28 +0800

    Add files via upload

--- gyroscope/gyroscope.h
+++ gyroscope/gyroscope.h
@@ -1,12 +1,12 @@
 #ifndef TOROI_NEW_H
 #define TOROI_NEW_H
-
+#include <SoftwareSerial.h>
 #include "Arduino.h"
 //2017/5/9 新增新功能 
 class gyroscope_mpu6050 
         { 
               public:
-              	gyroscope_mpu6050();
+              	gyroscope_mpu6050(int rx,int tx,int boundrate);
               	~gyroscope_mpu6050();
               	double x_axis;
               	double y_axis;
@@ -14,6 +14,7 @@ class gyroscope_mpu6050
               	double get_v();
               	
               private:
+                SoftwareSerial *gyroscope;
         		unsigned char Re_buf[11],counter=0;
                 unsigned char sign=0;
                 int check(int checked_distance);
